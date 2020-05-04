fddbd5ec937022708e369056dab6b8b75ca1ca17
Santiago Viquez <santi.viquez@gmail.com>
2020-05-04 12:54:25 -0600

    Add num_generate and temperature as parameters

--- model/predict_harry_potter.py
+++ model/predict_harry_potter.py
@@ -4,30 +4,24 @@ import os
 from rnn import build_model
 
 
-def generate_text(model, start_string):
-    # Evaluation step (generating text using the learned model)
+def generate_text(model, start_string, num_generate=1000, temperature=1.0):
+    '''Generates text using the learned model'''
 
-    # Number of characters to generate
-    num_generate = 1000
-
-    # Converting our start string to numbers (vectorizing)
+    # Converting our start string to numbers
     input_eval = [char2idx[s] for s in start_string]
     input_eval = tf.expand_dims(input_eval, 0)
 
     # Empty string to store our result
     text_generated = []
-
-    # Low temperatures results in more predictable text.
-    # Higher temperatures results in more surprising text.
-    temperature = 1.0
-
+    # Resets the state of metrics
     model.reset_states()
+
     for _ in range(num_generate):
         predictions = model(input_eval)
-        # remove the batch dimension
+        # Remove the batch dimension
         predictions = tf.squeeze(predictions, 0)
 
-        # using a categorical distribution to predict the character returned by the model
+        # Using a categorical distribution to predict the character returned by the model
         predictions = predictions / temperature
         predicted_id = tf.random.categorical(
             predictions, num_samples=1)[-1, 0].numpy()
@@ -46,9 +40,9 @@ text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 # Unique characters in the text
 vocab = sorted(set(text))
 
-# Vectorize the text
-# Create a mapping from unique characters to indices
+# Map each unique char to a different index
 char2idx = {u: i for i, u in enumerate(vocab)}
+# Map the index to the respective char
 idx2char = np.array(vocab)
 
 # Length of the vocabulary in charts
@@ -57,13 +51,19 @@ vocab_size = len(vocab)
 embedding_dim = 256
 # Number of RNN units
 rnn_units = 1024
+# Build network structure
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
 
-
-checkpoint_path = "training_checkpoints_hp/cp-{epoch:04d}.ckpt"
+checkpoint_path = "../training_checkpoints/cp-{epoch:04d}.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
-model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+# Load the weights of our latest learned model
 model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+# Build the learned model
 model.build(tf.TensorShape([1, None]))
 
-print(generate_text(model, start_string=u'ROMEO: '))
+# Make predictions
+predicted_text = generate_text(
+    model, start_string=u'Harry ', num_generate=1000, temperature=1.0)
+
+print(predicted_text)
